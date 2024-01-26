268ca075ad14204ba4a4b21d7b2b54f64c971296
Eduardo Pinheiro Miranda <89249898+EduardoPinheiroMiranda@users.noreply.github.com>
2024-01-25 23:30:06 -0300

    pegando id para selecionar qual é a paleta para favoritar

--- src/services/favoritarPaleta/index.ts
+++ src/services/favoritarPaleta/index.ts
@@ -1,10 +1,19 @@
 import { ModeloDeRequisicoesParaPaleta } from "@/repository/pelatas"
 
 
+interface PadraoDeDados{
+	id: string
+}
+
 export class FavoritarPaleta{
 	constructor(private requisicaoAoBanco: ModeloDeRequisicoesParaPaleta ){}
 
-	async execut(){
+	async execut({
+		id
+	}: PadraoDeDados){
         
+		const paleta = await this.requisicaoAoBanco.getById(id)
+
+		return paleta
 	}
 }
